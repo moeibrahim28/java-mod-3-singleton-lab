ab0804d5049f07656e4dec0d21364c4a4ae88415
moeibrahim28 <m.ibrahim0228@gmail.com>
2022-07-13 11:15:11 -0400

    sample output achieved

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        // your code here
+        Logger logger=Logger.getInstance();
+        logger.log("Sample output line 1");
+        logger.log("Second line of sample output");
     }
 }
+
